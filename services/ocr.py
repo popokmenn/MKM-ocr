699e4a9b5c5e82ac6b157cbd2685a77ebe3c7a0c
xti-fakhriazhar <Fakhri.Azhar@xapiens.id>
2025-06-25 11:51:56 +0700

    Adding resize image function and apply on the scan_engine_number. Add get_ocr_text function to get only the engine number. And change annotation because it would return text.

--- services/ocr.py
+++ services/ocr.py
@@ -20,7 +20,31 @@ ocr_engine = PaddleOCR(
 )
 
 
-def scan_engine_number(image_bytes: bytes) -> list[str]:
+def resize_image(image, max_width=1000, max_height=1000):
+    """Function to resize image for improving image processing"""
+    # Get current size
+    h, w = image.shape[:2]
+
+    # Compute scaling factor, preserve aspect ratio
+    scale = min(max_width / w, max_height / h, 1.0)  # don't upscale!
+    if scale < 1.0:
+        new_w, new_h = int(w * scale), int(h * scale)
+        image = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_AREA)
+
+    return image
+
+
+def get_ocr_text(texts: list) -> str:
+    """Function to validate whether the text from the image is engine number
+    """
+
+    # NOTE: This is a temporary validation, will be modified for actual implementation
+    ocr_text = [text for text in texts if (len(text) > 7)][0]
+
+    return ocr_text
+
+
+def scan_engine_number(image_bytes: bytes) -> str:
     """Function to use Paddle OCR to text in image
 
     Parameters
@@ -36,19 +60,24 @@ def scan_engine_number(image_bytes: bytes) -> list[str]:
     image = np.frombuffer(image_bytes, np.uint8)
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-    # NOTE: add preprocessing on the code below if needed
+    # NOTE: image preprocessing
+    # Resize image
+    image = resize_image(image=image)
 
     # Check whether the image can be read by opencv
     if not isinstance(image, np.ndarray):
         raise ValueError("Invalid or unreadable image format")
 
     try:
-    # Get the paddle ocr results if 
+    # Get the paddle ocr results if there are any 
         ocr_results = ocr_engine.predict(image)
         if ocr_results:
-            return ocr_results[0]["rec_texts"]
+            # Get engine number from detected texts
+            ocr_text = get_ocr_text(texts=ocr_results[0]["rec_texts"])
+
+            return ocr_text
         else:
-            return []
+            return ""
     # Exception to handle unexpected behaviour from Paddle OCR
     except Exception as e:
         raise RuntimeError(f"Paddle OCR error: {e}") from e
